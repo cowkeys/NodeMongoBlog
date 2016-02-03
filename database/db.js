59e11dd907b094125c1c04ed7f4ceeaf828ba438
cowkeys <cowkeys@outlook.com>
2016-02-03 13:20:45 +0800

    1234

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ckblog');//；连接数据库
+mongoose.connect('mongodb://heroku_jd1560p7:fkmhtfdn5fd9m3f3sbbe6ut3h5@ds055495.mongolab.com:55495/heroku_jd1560p7');//；连接数据库
 var db  =mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
 db.once('open', function() {
